c40a600e724979f0acd7271994ea379e63b1896b
Sergey Kalistratov <sergey.kalistratov@dualbootpartners.com>
2022-09-07 13:31:14 +0500

    Solved lists exercises

--- exercises/lists.py
+++ exercises/lists.py
@@ -1,5 +1,15 @@
 class ListExercise:
     @staticmethod
+    def max_value(input_list: list[int]) -> int:
+        max_value = input_list[0]
+
+        for value in input_list:
+            if value > max_value:
+                max_value = value
+
+        return max_value
+
+    @staticmethod
     def replace(input_list: list[int]) -> list[int]:
         """
         Заменить все положительные элементы целочисленного списка на максимальное значение
@@ -7,7 +17,26 @@ class ListExercise:
         :param input_list: Исходный список
         :return: Список с замененными элементами
         """
-        pass
+        if not input_list:
+            return []
+
+        max_value_item = ListExercise.max_value(input_list)
+
+        return list(map(lambda x: max_value_item if x > 0 else x, input_list))
+
+    @staticmethod
+    def recursive_search(input_list: list[int], query: int, min: int = 0, max: int = 0) -> int:
+        if min > max:
+            return -1
+
+        mid = (min + max) // 2
+
+        if input_list[mid] == query:
+            return mid
+        elif input_list[mid] > query:
+            return ListExercise.recursive_search(input_list, query, min, mid - 1)
+        else:
+            return ListExercise.recursive_search(input_list, query, mid + 1, max)
 
     @staticmethod
     def search(input_list: list[int], query: int) -> int:
@@ -18,4 +47,6 @@ class ListExercise:
         :param query: Искомый элемент
         :return: Номер элемента
         """
-        pass
+        min, max = 0, len(input_list) - 1
+
+        return ListExercise.recursive_search(input_list, query, min, max)
